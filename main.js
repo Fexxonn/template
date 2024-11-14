806cd66aee3ea34324e99d64d99d8a638f63f582
Fexxonn <torrenegradavid@gmail.com>
2024-11-14 08:28:55 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Abre y cierra las respuestas de las preguntas frecuentes
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        const faqItem = question.parentElement;
+        faqItem.classList.toggle('active');
+    });
+});
